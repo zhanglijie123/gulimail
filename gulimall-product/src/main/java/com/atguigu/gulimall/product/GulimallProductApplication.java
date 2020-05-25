ee45aa3f118507296cf214af24a20e9df6baa0d6
zhanglijie123 <1437044497@qq.com>
2020-05-25 18:18:05 +0800

    项目初始化搭建

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProductApplication {
+
+	public static void main(String[] args) {
+		System.out.println("prduct start");
+		SpringApplication.run(GulimallProductApplication.class, args);
+
+	}
+
+}
